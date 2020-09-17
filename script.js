132eda56b593641dfb5a5ff7ac46209499d201b3
AgataLub <agat0538@stud.kea.dk>
2020-09-17 14:39:50 +0200

    js

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-let url = 'https://api.sheety.co/2360d9d43907ee174c29980e403b34fa/foodData/foglio1';
-fetch(url)
-.then((response) => response.json())
-.then(json => {
-  // Do something with the data
-  console.log(json.foglio1);
-});
+//Fetching data
+
+const link = "https://spreadsheets.google.com/feeds/list/1V5Bdd4MSLfpuNOXcmQzrql5b5HB_RebrYXiOILyPJOw/od6/public/values?alt=json";
+window.addEventListener("DOMContentLoaded", getData);
+
+function getData() {
+    fetch(link)
+    .then(res => res.json())
+    .then(handleData);
+}
+
+function handleData(data) {
+    const myData = data.feed.entry;
+    console.log("myData console");
+    console.log(myData);
+    myData.forEach(showData);
+}
+
+function showData(singleRowData) {
+    console.log("singleRowData - console");
+    console.log(singleRowData.gsx$name.$t);
+
+    //Filling the template
+
+    const template = document.querySelector('template').content;
+    const clone = template.cloneNode(true);
+
+    const h3 = clone.querySelector('h3');
+    h3.textContent = singleRowData.gsx$name.$t;
+
+    document.querySelector("main").appendChild(clone);
+}
+
